d9547c1f36594740cddd883546e6aaa6e02357bc
Mateusz Judka <mrjudka@gmail.coM>
2023-08-18 00:23:35 +0100

    Added the start of weatherAPP

---
+++ AspMVC/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using static AspMVC.Models.WeatherViewModel;
+
+public class WeatherController : Controller
+{
+    public async Task<IActionResult> Windex()
+    {
+        string apiKey = "4a27f543c7d84c9aa46224819231708";
+        string city = "Birmingham"; // You can make this dynamic
+
+        using (HttpClient client = new HttpClient())
+        {
+            string apiUrl = $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}";
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(jsonData);
+                return View(weatherInfo);
+            }
+            else
+            {
+                // Handle the error
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ AspMVC/Models/WeatherViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace AspMVC.Models
+{
+    public class WeatherViewModel
+    {
+        public class WeatherInfo
+        {
+            public Location Location { get; set; }
+            public Current Current { get; set; }
+        }
+
+        public class Location
+        {
+            public string Name { get; set; }
+            public string Region { get; set; }
+            public string Country { get; set; }
+        }
+
+        public class Current
+        {
+            public double Temp_C { get; set; }
+            public int Humidity { get; set; }
+        }
+    }
+}
